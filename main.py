31a7921e78db55b3cdd407eb2b779640d1973885
oktaviaanisa003-ai <oktaviaanisa003@gmail.com>
2026-01-14 11:08:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+print("--- Kuis Python Sederhana ---")
+jawaban = input("Apa ibukota Indonesia? ").lower()
+
+if jawaban == "jakarta":
+    print("Benar sekali!")
+else:
+    print("Wah, masih kurang tepat. Jawabannya adalah Jakarta.")
